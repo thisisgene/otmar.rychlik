bc2ad8dbfdd423a20ede349edd3e41fd37880cea
thisisgene <emdo2000@gmail.com>
2016-10-15 20:35:00 +0200

    Initiated initiating work environment.

---
+++ page/app/routes.js
@@ -0,0 +1,32 @@
+var Projects = require('../models/project');
+
+    module.exports = function(app) {
+
+        // server routes ===========================================================
+        // handle things like api calls
+        // authentication routes
+
+        // sample api route
+        app.get('/api/nerds', function(req, res) {
+            // use mongoose to get all projects in the database
+            Project.find(function(err, projects) {
+
+                // if there is an error retrieving, send the error.
+                                // nothing after res.send(err) will execute
+                if (err)
+                    res.send(err);
+
+                res.json(projects); // return all nerds in JSON format
+            });
+        });
+
+        // route to handle creating goes here (app.post)
+        // route to handle delete goes here (app.delete)
+
+        // frontend routes =========================================================
+        // route to handle all angular requests
+        app.get('*', function(req, res) {
+            res.sendfile('./public/views/index.html'); // load our public/index.html file
+        });
+
+    };
