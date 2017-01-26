b887590dea38999f5f750ebaf84a03d3bbdd8946
thisisgene <emdo2000@gmail.com>
2017-01-26 17:58:39 +0100

    Added all.

---
+++ dart/first/lib/views/side_content/side_content.dart
@@ -0,0 +1,66 @@
+import 'package:angular2/angular2.dart';
+
+import 'dart:async';
+
+import 'dart:html';
+
+
+import 'package:first/models/project.dart';
+import 'package:first/services/project_service.dart';
+
+
+@Component(selector: 'side-content',
+    templateUrl: 'side_content.html',
+    styleUrls: const ['side_content.css'],
+    providers: const [ProjectService]
+)
+class SideContent implements OnInit {
+
+  List<Project> projects;
+
+  final ProjectService _projectService;
+  SideContent(this._projectService);
+
+  Future<Null> getProjects() async {
+    projects = await _projectService.getProjects();
+  }
+
+  @Output() EventEmitter<String> myEvent = new EventEmitter();
+
+  void selectProject(project) {
+    myEvent.emit(project);
+  }
+
+  void listenToClick() {
+
+    querySelector('.menu-list').onClick.listen((MouseEvent e) {
+
+      Element target = e.target;
+      ElementList allProjects = querySelectorAll('.project-element');
+
+      if (target.classes.contains('project-element')){
+        querySelectorAll('.active').classes.remove('active');
+        target.classes.add('active');
+
+
+        for (Element project in allProjects) {
+          print(project);
+          if (project != target) {
+            project.parent.parent.classes.add('hidden');
+          }
+        }
+
+      }
+      if (target.classes.contains('icon-hasChildren')){
+        target.parent.parent.classes.toggle('is-open');
+      }
+    });
+  }
+
+  void ngOnInit() {
+    getProjects();
+    listenToClick();
+  }
+
+}
+
