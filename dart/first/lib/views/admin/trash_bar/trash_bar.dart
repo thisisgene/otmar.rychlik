42b57cd7f42890e8112d8107e205579256fb1594
thisisgene <emdo2000@gmail.com>
2017-02-15 22:05:08 +0100

    Started working on trash.

--- dart/first/lib/views/admin/trash_bar/trash_bar.dart
+++ dart/first/lib/views/admin/trash_bar/trash_bar.dart
@@ -7,14 +7,17 @@ import 'package:first/views/admin/side_bar_left/side_bar_left.dart';
 import 'package:first/models/project.dart';
 import 'package:first/services/project_service.dart';
 
-//import '../../services/firebase_service.dart';
+import 'package:first/services/firebase_service.dart';
 
 @Component(selector: 'trash-bar',
     templateUrl: 'trash_bar.html',
-    styleUrls: const ['trash_bar.css'],
-    providers: const [TrashBar]
+    styleUrls: const ['trash_bar.css']
 )
 class TrashBar {
+
+  final FirebaseService fbService;
+
+  TrashBar(FirebaseService this.fbService);
 
   void expandTrash() {
     querySelector('trash-bar').classes.toggle('expand');
